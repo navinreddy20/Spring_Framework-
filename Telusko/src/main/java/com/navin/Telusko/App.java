be723a21be490625fa1fe147f9f6d10a835d20b6
Telusko <Telusko@SuperComputer>
2019-08-28 16:48:34 +0530

    Maven Tutorial Practical

---
+++ Telusko/src/main/java/com/navin/Telusko/App.java
@@ -0,0 +1,16 @@
+package com.navin.Telusko;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Hello World");
+    }
+}
